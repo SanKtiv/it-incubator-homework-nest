601037510436d34e7317bce1c306a9a60b93cf2d
SanKtiv <aktitorov@gmail.com>
2024-06-02 20:44:08 +0300

    add send in AuthService

--- src/features/auth/application/auth.service.ts
+++ src/features/auth/application/auth.service.ts
@@ -39,8 +39,8 @@ export class AuthService {
   }
 
   async send(email: string) {
-    const confirmationCode: string = uuidv4();
-    return this.emailAdapter.sendConfirmationCode(email, confirmationCode);
+    //const confirmationCode: string = uuidv4();
+    await this.emailAdapter.sendConfirmationCode(email, 'confirmationCode');
   }
 
   async resendConfirmCode(email: string): Promise<void> {
