09d8fd05242ca4c5053611ec0cd52af886a50c11
SanKtiv <aktitorov@gmail.com>
2024-06-29 10:47:21 +0300

    update devices.controller.ts and add log in deleteDeviceById

--- src/features/security/api/devices.controller.ts
+++ src/features/security/api/devices.controller.ts
@@ -12,32 +12,31 @@ import { CurrentUserId } from '../../auth/infrastructure/decorators/current-user
 import { DevicesService } from '../application/devices.service';
 import { paramIdIsMongoIdPipe } from '../../../infrastructure/pipes/validation.pipe';
 
-@Controller('security')
+@Controller('security/devices')
+@UseGuards(JWTRefreshAuthGuard)
 export class DevicesController {
   constructor(private readonly devicesService: DevicesService) {}
 
-  @Get('devices')
+  @Get()
   @HttpCode(200)
-  @UseGuards(JWTRefreshAuthGuard)
   async getDevices(@CurrentUserId() userId: string) {
     console.log('security/devices start');
     console.log('userId =', userId);
     return this.devicesService.findByUserId(userId);
   }
 
-  @Delete('devices/:deviceId')
+  @Delete(':deviceId')
   @HttpCode(204)
-  @UseGuards(JWTRefreshAuthGuard)
   async deleteDeviceById(
     @Param(':deviceId', paramIdIsMongoIdPipe) id: string,
     @CurrentUserId() userId: string,
   ) {
+    console.log('deleteDeviceById start')
     await this.devicesService.deleteDeviceCurrentUserByDeviceId(id, userId);
   }
 
-  @Delete('devices')
+  @Delete()
   @HttpCode(204)
-  @UseGuards(JWTRefreshAuthGuard)
   async deleteDevices(@RefreshTokenPayload() payload: any) {
     await this.devicesService.deleteAllDevicesWithoutCurrent(payload);
   }
