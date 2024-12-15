ff1f64e1afda059d6818738aaffbbec8b21dd505
SanKtiv <aktitorov@gmail.com>
2024-12-15 14:00:27 +0300

    add console.logs in getPostById

--- src/features/posts/api/posts.controller.ts
+++ src/features/posts/api/posts.controller.ts
@@ -67,7 +67,8 @@ export class PostController {
     const post = await this.postsQueryRepositorySql.findById_RAW(id, userId);
 
     if (!post) throw new NotFoundException();
-console.log('PostById =',post)
+console.log('PostById =',post.id)
+    console.log('PostById =',post.extendedLikesInfo.newestLikes)
     return post;
   }
 
