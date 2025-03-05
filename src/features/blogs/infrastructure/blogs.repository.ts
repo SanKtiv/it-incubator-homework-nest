6ce7e29a1700b30503942f47e79cbdfd47cd370c
SanKtiv <aktitorov@gmail.com>
2025-03-05 09:35:57 +0300

    refactoring blogs.repository.ts

--- src/features/blogs/infrastructure/blogs.repository.ts
+++ src/features/blogs/infrastructure/blogs.repository.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { BlogsRepositoryTypeOrm } from './postgresdb/blogs.repository-typeorm';
-import { BlogsServicesDto } from '../api/models/input/blogs.services.dto';
 import { BlogsTable } from '../domain/blog.entity';
 import { BlogsInputDto } from '../api/models/input/blogs.input.dto';
+import {UpdateResult} from "typeorm";
 
 @Injectable()
 export class BlogsRepository {
@@ -20,7 +20,7 @@ export class BlogsRepository {
     return this.blogsRepository.updateById(id, inputUpdate);
   }
 
-  async deleteBlogById(id: string) {
+  async deleteBlogById(id: string): Promise<UpdateResult | void> {
     return this.blogsRepository.deleteOne(id);
   }
 
