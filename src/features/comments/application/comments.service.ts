330ccdf51515fefb149b3fd92da35a7482353267
SanKtiv <aktitorov@gmail.com>
2025-03-05 09:48:12 +0300

    refactoring comments.service.ts

--- src/features/comments/application/comments.service.ts
+++ src/features/comments/application/comments.service.ts
@@ -3,21 +3,13 @@ import {
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
-import { CommentsRepositoryMongo } from '../infrastructure/mongodb/comments.repository-mongo';
-import { PostsRepositoryMongo } from '../../posts/infrastructure/mongodb/posts.repository-mongo';
-import { UsersRepositoryMongo } from '../../users/infrastructure/mongodb/users.repository-mongo';
 import {
   CommentOutputDto,
   commentModelOutput,
 } from '../api/models/output/comment.output.dto';
-import { CommentServiceDto } from '../api/models/input/comment-service.dto';
 import { CommentInputDto } from '../api/models/input/comment.input.dto';
 import { PostLikeStatusDto } from '../../posts/api/models/input/posts.input.dto';
-import { CommentDocument } from '../domain/comment.schema';
 import { PostsService } from '../../posts/application/posts.service';
-import { UsersRepositoryRawsql } from '../../users/infrastructure/postgresqldb/users.repository-rawsql';
-import { CommentsRepositorySql } from '../infrastructure/postgresql/comments.repository-sql';
-import { StatusesRepositorySql } from '../../statuses/infrastructure/postgresql/statuses.repository-sql';
 import { CommentsTable } from '../domain/comments.entity';
 import { CommentsRepository } from '../infrastructure/comments.repository';
 import {StatusesCommentsRepository} from "../../statuses/infrastructure/statuses.comments.repository";
@@ -46,7 +38,7 @@ export class CommentsService {
     return commentModelOutput(comment);
   }
 
-  async updateCommentById(id: string, userId: string, dto: CommentInputDto) {
+  async updateCommentById(id: string, userId: string, dto: CommentInputDto): Promise<void> {
     const comment = await this.existComment(id);
 
     if (comment.userId !== userId) throw new ForbiddenException();
@@ -56,7 +48,7 @@ export class CommentsService {
     await this.commentsRepository.updateComment(comment);
   }
 
-  async deleteCommentById(id: string, userId: string) {
+  async deleteCommentById(id: string, userId: string): Promise<void> {
     const comment = await this.existComment(id);
 
     if (comment.userId !== userId) throw new ForbiddenException();
@@ -87,71 +79,4 @@ export class CommentsService {
 
     return comment;
   }
-
-  // async createLikeStatus(id: string, userId: string, dto: PostLikeStatusDto) {
-  //   const commentDocument = await this.commentsRepository.findById(id);
-  //
-  //   if (!commentDocument) throw new NotFoundException();
-  //
-  //   const newStatus = dto.likeStatus;
-  //
-  //   const currentUser = commentDocument.usersStatuses.find(
-  //       (e) => e.userId === userId,
-  //   );
-  //
-  //   if (newStatus === 'None' && !currentUser) return;
-  //
-  //   if (newStatus === 'None' && currentUser) {
-  //     if (currentUser.userStatus === 'Like') commentDocument.likesCount--;
-  //
-  //     if (currentUser.userStatus === 'Dislike') commentDocument.dislikesCount--;
-  //
-  //     commentDocument.usersStatuses = commentDocument.usersStatuses.filter(
-  //         (e) => e.userId !== userId,
-  //     );
-  //
-  //     await this.commentsRepository.save(commentDocument);
-  //
-  //     return;
-  //   }
-  //
-  //   const userStatus = {
-  //     userId: userId,
-  //     userStatus: dto.likeStatus,
-  //   };
-  //
-  //   const newStatusIsLike = newStatus === 'Like';
-  //
-  //   const newStatusIsDislike = newStatus === 'Dislike';
-  //
-  //   if (!currentUser) {
-  //     if (newStatusIsLike) commentDocument.likesCount++;
-  //
-  //     if (newStatusIsDislike) commentDocument.dislikesCount++;
-  //
-  //     commentDocument.usersStatuses.push(userStatus);
-  //
-  //     await this.commentsRepository.save(commentDocument);
-  //
-  //     return;
-  //   }
-  //
-  //   if (newStatusIsLike && currentUser.userStatus === 'Dislike') {
-  //     commentDocument.likesCount++;
-  //     commentDocument.dislikesCount--;
-  //   }
-  //
-  //   if (newStatusIsDislike && currentUser.userStatus === 'Like') {
-  //     commentDocument.likesCount--;
-  //     commentDocument.dislikesCount++;
-  //   }
-  //
-  //   commentDocument.usersStatuses = commentDocument.usersStatuses.map((e) =>
-  //       e.userId === userId ? userStatus : e,
-  //   );
-  //
-  //   await this.commentsRepository.save(commentDocument);
-  //
-  //   return;
-  // }
 }
