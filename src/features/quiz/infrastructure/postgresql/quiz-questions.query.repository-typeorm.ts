efcad3ab48305e61d40701f5e9cf04ee164c8cb6
SanKtiv <aktitorov@gmail.com>
2025-03-14 15:05:58 +0300

    add QuizQuestionsQueryRepositoryTypeOrm

--- src/features/quiz/infrastructure/postgresql/quiz-questions.query.repository-typeorm.ts
+++ src/features/quiz/infrastructure/postgresql/quiz-questions.query.repository-typeorm.ts
@@ -1,7 +1,7 @@
 import {Injectable} from "@nestjs/common";
 
 @Injectable()
-export class QuizQuestionsQueryRepositoryTypeorm {
+export class QuizQuestionsQueryRepositoryTypeOrm {
     constructor() {
     }
 
