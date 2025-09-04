c42119e162801bfe6a03dc48f12fc611eba2ed27
SanKtiv <aktitorov@gmail.com>
2025-09-04 11:44:56 +0300

    add .addOrderBy('firstPlayerAnswers.addedAt', 'DESC')                 .addOrderBy('secondPlayerAnswers.addedAt', 'DESC') in newGetPaging

--- src/features/quiz/pair-game/infrastucture/postgresq/pair-game.query.repository-typeorm.ts
+++ src/features/quiz/pair-game/infrastucture/postgresq/pair-game.query.repository-typeorm.ts
@@ -211,6 +211,8 @@ export class PairGameQueryRepositoryTypeOrm {
         }
 
         return gamesPaging
+            .addOrderBy('firstPlayerAnswers.addedAt', 'DESC')
+            .addOrderBy('secondPlayerAnswers.addedAt', 'DESC')
             .addOrderBy('questions.index', 'ASC')
             .getMany();
     }
