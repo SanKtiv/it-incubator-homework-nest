95184b484a6e2d5844ecf4c23eaef8249e5b436f
SanKtiv <aktitorov@gmail.com>
2025-09-15 15:54:49 +0300

    refactoring

--- src/features/quiz/pair-game/infrastucture/postgresq/pair-game.query.repository-typeorm.ts
+++ src/features/quiz/pair-game/infrastucture/postgresq/pair-game.query.repository-typeorm.ts
@@ -1,47 +1,49 @@
-import { Injectable } from '@nestjs/common';
-import { InjectDataSource, InjectRepository } from '@nestjs/typeorm';
-import { QuizPairGameEntity } from '../../domain/pair-game.entity';
-import { DataSource, Repository } from 'typeorm';
-import { pairGameQuery } from '../../api/models/input/input-query.dto';
+import {Injectable} from '@nestjs/common';
+import {InjectDataSource, InjectRepository} from '@nestjs/typeorm';
+import {QuizPairGameEntity} from '../../domain/pair-game.entity';
+import {DataSource, Repository} from 'typeorm';
+import {pairGameQuery} from '../../api/models/input/input-query.dto';
 import {NewPairGameEntity} from "../../domain/new-pair-game.entity";
 
 @Injectable()
 export class PairGameQueryRepositoryTypeOrm {
-  constructor(
-    @InjectRepository(QuizPairGameEntity)
-    protected repository: Repository<QuizPairGameEntity>,
-    @InjectRepository(NewPairGameEntity)
-    protected newRepository: Repository<NewPairGameEntity>,
-    @InjectDataSource() protected dataSource: DataSource,
-  ) {}
-  private get building() {
-    return this.repository
-      .createQueryBuilder('pg')
-      .leftJoinAndSelect('pg.firstPlayer', 'firstPlayer')
-      .leftJoinAndSelect('firstPlayer.accountData', 'firstAccountData')
-      .leftJoinAndSelect('pg.secondPlayer', 'secondPlayer')
-      .leftJoinAndSelect('secondPlayer.accountData', 'secondAccountData')
-      .select([
-        'pg',
-        'firstPlayer.id',
-        'secondPlayer.id',
-        'firstAccountData.login',
-        'secondAccountData.login',
-      ])
-      .leftJoinAndSelect(
-        'pg.answersFirstPlayer',
-        'answersFirstPlayer',
-        'firstPlayer.id = answersFirstPlayer.userId',
-      )
-      .leftJoinAndSelect(
-        'pg.answersSecondPlayer',
-        'answersSecondPlayer',
-        'secondPlayer.id = answersSecondPlayer.userId',
-      )
-      .leftJoinAndSelect('pg.questions', 'questions');
-  }
+    constructor(
+        @InjectRepository(QuizPairGameEntity)
+        protected repository: Repository<QuizPairGameEntity>,
+        @InjectRepository(NewPairGameEntity)
+        protected newRepository: Repository<NewPairGameEntity>,
+        @InjectDataSource() protected dataSource: DataSource,
+    ) {
+    }
 
-  private get shareBuilder() {
+    // private get building_OLD() {
+    //     return this.repository
+    //         .createQueryBuilder('pg')
+    //         .leftJoinAndSelect('pg.firstPlayer', 'firstPlayer')
+    //         .leftJoinAndSelect('firstPlayer.accountData', 'firstAccountData')
+    //         .leftJoinAndSelect('pg.secondPlayer', 'secondPlayer')
+    //         .leftJoinAndSelect('secondPlayer.accountData', 'secondAccountData')
+    //         .select([
+    //             'pg',
+    //             'firstPlayer.id',
+    //             'secondPlayer.id',
+    //             'firstAccountData.login',
+    //             'secondAccountData.login',
+    //         ])
+    //         .leftJoinAndSelect(
+    //             'pg.answersFirstPlayer',
+    //             'answersFirstPlayer',
+    //             'firstPlayer.id = answersFirstPlayer.userId',
+    //         )
+    //         .leftJoinAndSelect(
+    //             'pg.answersSecondPlayer',
+    //             'answersSecondPlayer',
+    //             'secondPlayer.id = answersSecondPlayer.userId',
+    //         )
+    //         .leftJoinAndSelect('pg.questions', 'questions');
+    // }
+
+    private get shareBuilder() {
         return this.newRepository
             .createQueryBuilder('pg')
             .select(['pg'])
@@ -51,14 +53,6 @@ export class PairGameQueryRepositoryTypeOrm {
             .leftJoinAndSelect('pg.secondPlayer', 'secondPlayer')
             .leftJoinAndSelect('secondPlayer.user', 'secondUser')
             .leftJoinAndSelect('secondUser.accountData', 'secondAccountData')
-            //.addSelect(['firstPlayer.id'])
-            // .select([
-            //     'pg',
-            //     'user.id',
-            //     'secondPlayer.userId',
-            //     'firstAccountData.login',
-            //     'secondAccountData.login',
-            // ])
             .leftJoinAndSelect(
                 'firstPlayer.answers',
                 'firstPlayerAnswers',
@@ -74,28 +68,11 @@ export class PairGameQueryRepositoryTypeOrm {
             .orderBy('questions.index', 'ASC');
     }
 
-  async getById_OLD(id: string): Promise<QuizPairGameEntity | null> {
-    return this.building
-      .where('pg."id" = :id', { id })
-      .orderBy('questions.id', 'ASC')
-      .getOne();
-  }
-
-  async getById(id: string): Promise<NewPairGameEntity | null> {
+    async getById(id: string): Promise<NewPairGameEntity | null> {
         return this.shareBuilder
-            .where('pg."id" = :id', { id })
+            .where('pg."id" = :id', {id})
             .getOne();
     }
-
-  async getByUserId_OLD(userId: string): Promise<QuizPairGameEntity | null> {
-    return this.building
-      .where('pg.finishGameDate IS NULL')
-      .andWhere('pg.firstPlayer.id = :userId', { userId })
-      .orWhere('pg.finishGameDate IS NULL')
-      .andWhere('pg.secondPlayer.id = :userId', { userId })
-      .orderBy('questions.id', 'ASC')
-      .getOne();
-  }
 
     async getByUserId(userId: string): Promise<NewPairGameEntity | null> {
         return this.shareBuilder
@@ -103,64 +80,12 @@ export class PairGameQueryRepositoryTypeOrm {
             .andWhere('firstUser.id = :userId')
             .orWhere('pg.finishGameDate IS NULL')
             .andWhere('secondUser.id = :userId')
-            .setParameters({ userId })
+            .setParameters({userId})
             .orderBy('questions.index', 'ASC')
             .getOne();
     }
 
-  async getPaging_OLD(
-    userId: string,
-    query: pairGameQuery,
-  ): Promise<QuizPairGameEntity[]> {
-    const idsSubQuery = this.repository
-      .createQueryBuilder('pg')
-      .select('pg.id')
-      .where('pg.firstPlayer.id = :userId')
-      .orWhere('pg.secondPlayer.id = :userId')
-      .setParameters({ userId })
-      .orderBy(`"${query.sortBy}"`, query.sortDirection)
-      .skip((query.pageNumber - 1) * query.pageSize)
-      .take(query.pageSize);
-
-    if (query.sortBy !== 'pairCreatedDate') {
-      idsSubQuery.addOrderBy('pg."pairCreatedDate"', 'DESC');
-    }
-
-    const gamesPaging = this.repository
-      .createQueryBuilder('pg')
-      .where(`pg.id IN (${idsSubQuery.getQuery()})`)
-      .setParameters(idsSubQuery.getParameters())
-      .leftJoinAndSelect('pg.firstPlayer', 'firstPlayer')
-      .leftJoinAndSelect(
-        'pg.answersFirstPlayer',
-        'answersFirstPlayer',
-        'answersFirstPlayer.userId = firstPlayer.id',
-      )
-      .leftJoinAndSelect('firstPlayer.accountData', 'firstAccountData')
-      .leftJoinAndSelect('pg.secondPlayer', 'secondPlayer')
-      .leftJoinAndSelect(
-        'pg.answersSecondPlayer',
-        'answersSecondPlayer',
-        'answersSecondPlayer.userId = secondPlayer.id',
-      )
-      .leftJoinAndSelect('secondPlayer.accountData', 'secondAccountData')
-      .leftJoinAndSelect('pg.questions', 'questions')
-      .orderBy(`"${query.sortBy}"`, query.sortDirection);
-
-    if (query.sortBy !== 'pairCreatedDate') {
-      return gamesPaging
-        .addOrderBy('pg."pairCreatedDate"', 'DESC')
-        .addOrderBy('questions.id', 'ASC')
-        .getMany();
-    }
-
-    return gamesPaging.addOrderBy('questions.id', 'ASC').getMany();
-  }
-
-    async getPaging(
-        userId: string,
-        query: pairGameQuery,
-    ): Promise<NewPairGameEntity[]> {
+    async getPaging(userId: string, query: pairGameQuery): Promise<NewPairGameEntity[]> {
         const idsSubQuery = this.newRepository
             .createQueryBuilder('pg')
             .select('pg.id')
@@ -170,7 +95,7 @@ export class PairGameQueryRepositoryTypeOrm {
             .leftJoin('secondPlayer.user', 'secondUser')
             .where('firstUser.id = :userId')
             .orWhere('secondUser.id = :userId')
-            .setParameters({ userId })
+            .setParameters({userId})
             .skip((query.pageNumber - 1) * query.pageSize)
             .take(query.pageSize);
 
@@ -204,16 +129,6 @@ export class PairGameQueryRepositoryTypeOrm {
             .getMany();
     }
 
-  async getTotalGamesByUserId_OLD(userId: string): Promise<number> {
-    return this.repository
-      .createQueryBuilder('pg')
-      .select('pg.id')
-      .where('pg.firstPlayer.id = :userId')
-      .orWhere('pg.secondPlayer.id = :userId')
-      .setParameters({ userId })
-      .getCount();
-  }
-
     async getTotalGamesByUserId(userId: string): Promise<number> {
         return this.newRepository
             .createQueryBuilder('pg')
@@ -224,50 +139,123 @@ export class PairGameQueryRepositoryTypeOrm {
             .leftJoinAndSelect('secondPlayer.user', 'secondUser')
             .where('firstUser.id = :userId')
             .orWhere('secondUser.id = :userId')
-            .setParameters({ userId })
+            .setParameters({userId})
             .getCount();
     }
 
-  async getStatisticByUserId_OLD(userId: string): Promise<QuizPairGameEntity[]> {
-    return this.repository
-      .createQueryBuilder('pg')
-      .leftJoinAndSelect('pg.firstPlayer', 'firstPlayer')
-      .leftJoinAndSelect('pg.secondPlayer', 'secondPlayer')
-      .select([
-        'pg.status',
-        'pg.firstPlayerScore',
-        'pg.secondPlayerScore',
-        'firstPlayer.id',
-        'secondPlayer.id',
-      ])
-      .where('pg.firstPlayer.id = :userId', { userId })
-      .orWhere('pg.secondPlayer.id = :userId', { userId })
-      .getMany();
-  }
-
     async getStatisticByUserId(userId: string) {
-      try {
-          const res = await this.newRepository
-              .createQueryBuilder('pg')
-              .leftJoinAndSelect('pg.firstPlayer', 'firstPlayer')
-              .leftJoinAndSelect('firstPlayer.user', 'firstUser')
-              .leftJoinAndSelect('pg.secondPlayer', 'secondPlayer')
-              .leftJoinAndSelect('secondPlayer.user', 'secondUser')
-              .select([
-                  'pg.status',
-                  'firstPlayer.playerScore',
-                  'secondPlayer.playerScore',
-                  'firstUser.id',
-                  'secondUser.id',
-              ])
-              .where('firstUser.id = :userId')
-              .orWhere('secondUser.id = :userId')
-              .setParameters({ userId })
-              .getMany();
+        try {
+            const res = await this.newRepository
+                .createQueryBuilder('pg')
+                .leftJoinAndSelect('pg.firstPlayer', 'firstPlayer')
+                .leftJoinAndSelect('firstPlayer.user', 'firstUser')
+                .leftJoinAndSelect('pg.secondPlayer', 'secondPlayer')
+                .leftJoinAndSelect('secondPlayer.user', 'secondUser')
+                .select([
+                    'pg.status',
+                    'firstPlayer.playerScore',
+                    'secondPlayer.playerScore',
+                    'firstUser.id',
+                    'secondUser.id',
+                ])
+                .where('firstUser.id = :userId')
+                .orWhere('secondUser.id = :userId')
+                .setParameters({userId})
+                .getMany();
 
-          return res;
-      } catch (e) {
-          console.log('ERROR in newGetStatisticByUserId', e)
-      }
+            return res;
+        } catch (e) {
+            console.log('ERROR in newGetStatisticByUserId', e)
+        }
     }
+
+    // async getById_OLD(id: string): Promise<QuizPairGameEntity | null> {
+    //     return this.building
+    //         .where('pg."id" = :id', {id})
+    //         .orderBy('questions.id', 'ASC')
+    //         .getOne();
+    // }
+    //
+    // async getByUserId_OLD(userId: string): Promise<QuizPairGameEntity | null> {
+    //     return this.building
+    //         .where('pg.finishGameDate IS NULL')
+    //         .andWhere('pg.firstPlayer.id = :userId', {userId})
+    //         .orWhere('pg.finishGameDate IS NULL')
+    //         .andWhere('pg.secondPlayer.id = :userId', {userId})
+    //         .orderBy('questions.id', 'ASC')
+    //         .getOne();
+    // }
+    //
+    // async getPaging_OLD(userId: string, query: pairGameQuery): Promise<QuizPairGameEntity[]> {
+    //     const idsSubQuery = this.repository
+    //         .createQueryBuilder('pg')
+    //         .select('pg.id')
+    //         .where('pg.firstPlayer.id = :userId')
+    //         .orWhere('pg.secondPlayer.id = :userId')
+    //         .setParameters({userId})
+    //         .orderBy(`"${query.sortBy}"`, query.sortDirection)
+    //         .skip((query.pageNumber - 1) * query.pageSize)
+    //         .take(query.pageSize);
+    //
+    //     if (query.sortBy !== 'pairCreatedDate') {
+    //         idsSubQuery.addOrderBy('pg."pairCreatedDate"', 'DESC');
+    //     }
+    //
+    //     const gamesPaging = this.repository
+    //         .createQueryBuilder('pg')
+    //         .where(`pg.id IN (${idsSubQuery.getQuery()})`)
+    //         .setParameters(idsSubQuery.getParameters())
+    //         .leftJoinAndSelect('pg.firstPlayer', 'firstPlayer')
+    //         .leftJoinAndSelect(
+    //             'pg.answersFirstPlayer',
+    //             'answersFirstPlayer',
+    //             'answersFirstPlayer.userId = firstPlayer.id',
+    //         )
+    //         .leftJoinAndSelect('firstPlayer.accountData', 'firstAccountData')
+    //         .leftJoinAndSelect('pg.secondPlayer', 'secondPlayer')
+    //         .leftJoinAndSelect(
+    //             'pg.answersSecondPlayer',
+    //             'answersSecondPlayer',
+    //             'answersSecondPlayer.userId = secondPlayer.id',
+    //         )
+    //         .leftJoinAndSelect('secondPlayer.accountData', 'secondAccountData')
+    //         .leftJoinAndSelect('pg.questions', 'questions')
+    //         .orderBy(`"${query.sortBy}"`, query.sortDirection);
+    //
+    //     if (query.sortBy !== 'pairCreatedDate') {
+    //         return gamesPaging
+    //             .addOrderBy('pg."pairCreatedDate"', 'DESC')
+    //             .addOrderBy('questions.id', 'ASC')
+    //             .getMany();
+    //     }
+    //
+    //     return gamesPaging.addOrderBy('questions.id', 'ASC').getMany();
+    // }
+    //
+    // async getTotalGamesByUserId_OLD(userId: string): Promise<number> {
+    //     return this.repository
+    //         .createQueryBuilder('pg')
+    //         .select('pg.id')
+    //         .where('pg.firstPlayer.id = :userId')
+    //         .orWhere('pg.secondPlayer.id = :userId')
+    //         .setParameters({userId})
+    //         .getCount();
+    // }
+    //
+    // async getStatisticByUserId_OLD(userId: string): Promise<QuizPairGameEntity[]> {
+    //     return this.repository
+    //         .createQueryBuilder('pg')
+    //         .leftJoinAndSelect('pg.firstPlayer', 'firstPlayer')
+    //         .leftJoinAndSelect('pg.secondPlayer', 'secondPlayer')
+    //         .select([
+    //             'pg.status',
+    //             'pg.firstPlayerScore',
+    //             'pg.secondPlayerScore',
+    //             'firstPlayer.id',
+    //             'secondPlayer.id',
+    //         ])
+    //         .where('pg.firstPlayer.id = :userId', {userId})
+    //         .orWhere('pg.secondPlayer.id = :userId', {userId})
+    //         .getMany();
+    // }
 }
