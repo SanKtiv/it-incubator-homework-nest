52667054bac4425f1f8a7f03523567374d8de024
SanKtiv <aktitorov@gmail.com>
2025-09-22 15:26:38 +0300

    add GameQueryTopUsers

--- src/features/quiz/pair-game/api/models/input/input-query.dto.ts
+++ src/features/quiz/pair-game/api/models/input/input-query.dto.ts
@@ -1,8 +1,31 @@
 import { QueryDto } from '../../../../../../infrastructure/models/query.dto';
-import { IsOptional, IsString } from 'class-validator';
+import {IsNumber, IsOptional, IsString, Min} from 'class-validator';
+import {Type} from "class-transformer";
 
 export class pairGameQuery extends QueryDto {
   @IsOptional()
   @IsString()
   sortBy: string = 'pairCreatedDate';
+}
+
+export class GameQueryTopUsers {
+  constructor(query: Partial<QueryDto> = {}) {
+    Object.assign(this, query);
+  }
+
+  @IsOptional()
+  @IsString()
+  sort: string | string[] = ['avgScores desc', 'sumScore desc'];
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  @Min(0)
+  pageNumber: number = 1;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  @Min(0)
+  pageSize: number = 10;
 }
