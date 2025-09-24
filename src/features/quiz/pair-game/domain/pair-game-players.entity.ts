7485cf1dab05c5e40abd1960f2b65c9097a942ad
SanKtiv <aktitorov@gmail.com>
2025-09-24 09:23:44 +0300

    change PairGamePlayersEntity

--- src/features/quiz/pair-game/domain/pair-game-players.entity.ts
+++ src/features/quiz/pair-game/domain/pair-game-players.entity.ts
@@ -1,32 +1,47 @@
 import {
-  Column,
-  Entity,
-  JoinColumn,
-  ManyToOne,
-  OneToMany,
-  PrimaryGeneratedColumn,
+    Column,
+    Entity,
+    JoinColumn,
+    ManyToOne,
+    OneToMany,
+    PrimaryGeneratedColumn,
 } from 'typeorm';
-import { UsersTable } from '../../../users/domain/users.table';
-import { PlayerAnswersEntity } from './new-player-answers.entity';
+import {UsersTable} from '../../../users/domain/users.table';
+import {PlayerAnswersEntity} from './new-player-answers.entity';
 
 @Entity('pair-game-players')
 export class PairGamePlayersEntity {
-  @PrimaryGeneratedColumn('uuid')
-  id: string;
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
 
-  @ManyToOne(() => UsersTable, (user) => user.pairGamePlayer)
-  @JoinColumn()
-  user: UsersTable;
+    @ManyToOne(() => UsersTable, (user) => user.pairGamePlayer)
+    @JoinColumn()
+    user: UsersTable;
 
-  @OneToMany(() => PlayerAnswersEntity, (answer) => answer.player, {
-    nullable: true,
-    cascade: true,
-    eager: true,
-    onDelete: 'CASCADE',
-  })
-  @JoinColumn()
-  answers: PlayerAnswersEntity[] | null;
+    @OneToMany(() => PlayerAnswersEntity, (answer) => answer.player, {
+        nullable: true,
+        cascade: true,
+        eager: true,
+        onDelete: 'CASCADE',
+    })
+    @JoinColumn()
+    answers: PlayerAnswersEntity[] | null;
 
-  @Column({ type: 'smallint', default: 0 })
-  playerScore: number;
+    @Column({type: 'smallint', default: 0})
+    playerScore: number;
+
+    @Column({type: 'smallint', default: 0})
+    gamesCount: number;
+
+    @Column({type: 'double precision', default: 0})
+    avgScores: number;
+
+    @Column({type: 'smallint', default: 0})
+    winsCount: number;
+
+    @Column({type: 'smallint', default: 0})
+    lossesCount: number;
+
+    @Column({type: 'smallint', default: 0})
+    drawsCount: number;
 }
