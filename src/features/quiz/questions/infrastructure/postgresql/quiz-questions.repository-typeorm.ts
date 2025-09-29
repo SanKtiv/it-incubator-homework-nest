6b98a7e528c846c247e53acdc4f04b39135fae39
SanKtiv <aktitorov@gmail.com>
2025-09-29 14:30:27 +0300

    delete comments in quiz-questions.repository-typeorm.ts

--- src/features/quiz/questions/infrastructure/postgresql/quiz-questions.repository-typeorm.ts
+++ src/features/quiz/questions/infrastructure/postgresql/quiz-questions.repository-typeorm.ts
@@ -1,15 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { QuizQuestionsInputDto } from '../../api/models/quiz-questions.input.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import {QuestionsEntity,
-  // QuizQuestionsEntity
-} from '../../domain/quiz-questions.entity';
+import {QuestionsEntity} from '../../domain/quiz-questions.entity';
 import { Repository } from 'typeorm';
 
 @Injectable()
 export class QuizQuestionsRepositoryTypeOrm {
   constructor(
-    // @InjectRepository(QuizQuestionsEntity) protected repository_OLD: Repository<QuizQuestionsEntity>,
     @InjectRepository(QuestionsEntity) protected repository: Repository<QuestionsEntity>,
   ) {}
 
@@ -41,34 +37,4 @@ export class QuizQuestionsRepositoryTypeOrm {
   async clear(): Promise<void> {
     await this.repository.query('TRUNCATE TABLE "new_quiz-questions" CASCADE');
   }
-
-  // async insert_OLD(dto: QuizQuestionsEntity): Promise<QuizQuestionsEntity> {
-  //   return this.repository_OLD.save(dto);
-  // }
-
-  // async findFiveRandom_OLD(): Promise<QuizQuestionsEntity[]> {
-  //   return this.repository_OLD
-  //     .createQueryBuilder('q')
-  //     .select('q.*')
-  //     .orderBy('RANDOM()')
-  //     .addOrderBy('id', 'ASC')
-  //     .limit(5)
-  //     .getRawMany();
-  // }
-
-  // async findOneById_OLD(id: string): Promise<QuizQuestionsEntity | null> {
-  //   return this.repository_OLD.findOneBy({ id });
-  // }
-  //
-  // async update_OLD(dto: QuizQuestionsEntity) {
-  //   await this.repository_OLD.save(dto);
-  // }
-  //
-  // async softRemove_OLD(QuizQuestion: QuizQuestionsEntity): Promise<void> {
-  //   await this.repository_OLD.softRemove(QuizQuestion);
-  // }
-  //
-  // async clear_OLD(): Promise<void> {
-  //   await this.repository_OLD.query('TRUNCATE TABLE "quiz-questions" CASCADE');
-  // }
 }
