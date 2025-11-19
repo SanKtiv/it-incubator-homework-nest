2fcced669216ce95917e9c322387356e26a4d17f
SanKtiv <aktitorov@gmail.com>
2025-11-19 16:07:05 +0300

    update main

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,29 @@ import { AppModule } from './app.module';
 import { applyAppSettings } from './settings/apply-app-setting';
 import { appSettings } from './settings/app-settings';
 
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//
+//   applyAppSettings(app);
+//
+//   await app.listen(appSettings.api.APP_PORT, () => {
+//     console.log('App starting listen port: ', appSettings.api.APP_PORT);
+//     console.log('ENV: ', appSettings.env.getEnv());
+//   });
+// }
+//
+// bootstrap();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   applyAppSettings(app);
 
-  await app.listen(appSettings.api.APP_PORT, () => {
-    console.log('App starting listen port: ', appSettings.api.APP_PORT);
-    console.log('ENV: ', appSettings.env.getEnv());
-  });
+  await app.listen(appSettings.api.APP_PORT, '0.0.0.0');
+
+  console.log('App starting listen port:', appSettings.api.APP_PORT);
+  console.log('ENV:', appSettings.env.getEnv());
 }
 
 bootstrap();
+
